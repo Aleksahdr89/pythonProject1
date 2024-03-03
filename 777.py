092184a1cac9473aa39f8d50d98c9ddaa0334605
Aleksahdr89 <118809719+Aleksahdr89@users.noreply.github.com>
2024-03-03 15:21:13 +0300

    Создание угадайки

--- 777.py
+++ 777.py
@@ -1,1 +1,11 @@
-print('?ssddsd')
+from random import *
+number = randint(1, 100)
+number_user = bool
+print('Попробуй отгадать число в диапазоне от 1 до 5 включительно :')
+while number != number_user:
+    number_user = int(input())
+    if number_user > number:
+        print('Слишком много, попробуйте еще раз')
+    elif number_user < number:
+        print('Слишком мало, попробуйте еще раз')
+print('Вы угадали, поздравляем!')
